fe24179ff855fc9e5d44194cfffec038ab4daf6e
Graham Dyer <gdyer2@illinois.edu>
2014-09-14 15:37:09 -0500

    Update pull.py

--- Resources/pull.py
+++ Resources/pull.py
@@ -2,7 +2,7 @@ import os
 from os.path import expanduser
 home = expanduser("~")
 
-to_cd = home + "/Desktop/newsmapper.github.io"
+to_cd = home + "/AndroidStudioProjects/NewsReader"
 print(to_cd)
 os.chdir(to_cd)
 os.system("git pull https://github.com/newsmapper/newsmapper.github.io")
