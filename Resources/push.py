2fdd500b895a8a9efc51165cfe60aa0cbc81d6c8
Graham Dyer <gdyer2@illinois.edu>
2014-09-14 15:36:52 -0500

    Update push.py

--- Resources/push.py
+++ Resources/push.py
@@ -1,12 +1,12 @@
 import os
 from os.path import expanduser
 home = expanduser("~")
-to_cd = home + "/Desktop/newsmapper.github.io"
+to_cd = home + "/AndroidStudioProjects/NewsReader"
 os.chdir(to_cd)
 
 auto_pull = input('Auto-pull now? (y/anything): ')
 if auto_pull is 'y':
-	os.system("git pull https://github.com/NewsMapper/newsmapper.github.io")
+	os.system("git pull https://github.com/NewsMapper/Android-Client")
 auto_commit = input('Commit message (type "q" to quit): ')
 if auto_commit is 'q':
 	raise SystemExit
@@ -17,5 +17,5 @@ for foo in files:
 
 os.system("git commit -m '" + auto_commit.strip("'") + "'")
 os.system("git remote rm origin")
-os.system("git remote add origin https://github.com/NewsMapper/newsmapper.github.io")
+os.system("git remote add origin https://github.com/NewsMapper/Android-Client")
 os.system("git push origin master")
